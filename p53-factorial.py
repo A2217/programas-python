bfc8de0eaa9a6b7948cac1409ba1b510b145a47d
A2217 <armoal2008@hotmail.com>
2024-09-25 03:33:23 -0600

    Actividad 9

--- p53-factorial.py
+++ p53-factorial.py
@@ -4,7 +4,11 @@ print("Calcula el factorial de un número \n")
 
 n = int(input("Ingresa el número del cuál deseas factorial? "))
 f = 1
-for i in range(1,n+1):
+print(f"{n}! = ", end=" ")
+
+for i in range(1, n+1):
     f = f * i
+    print(f"{i} {"x" if i!=n else ""}",end =" ")
+    
 
 print(f"\nEl factorial es: {f}")
