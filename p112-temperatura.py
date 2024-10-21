4caae67c80d6f34ce360fd3f35065a2aa36ce94d
A2217 <armoal2008@hotmail.com>
2024-10-21 13:16:43 -0600

    Actividad 15

--- p112-temperatura.py
+++ p112-temperatura.py
@@ -6,10 +6,10 @@ def centigrados(temp):
     return ( temp-32 ) * (5/9)
 print('[1] Convertir a Fahrenheit')
 print('[2] Convertir a Centigrados')
-op = int(input("Elige ?"))
+op = int(input("Elige una opcion: "))
 if op==1 :
     temp = int(input('Dame una temperatura en Centigrados? '))
-    print(f'La temperatura en grados Fahrenheit es {farenheit(temp)}')
+    print(f'La temperatura en grados Fahrenheit es {fahrenheit(temp)}')
 elif op==2:
     temp = int(input('Dame una temperatura en Fahrenheit? '))
     print(f'La temperatura en grados Centigrados es {centigrados(temp)}')
